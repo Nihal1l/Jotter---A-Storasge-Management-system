b9351295f972aa5be18727e5dc64abe8c748e4ab
Nihal <p.b.nihalcse1@gmail.com>
2026-01-16 22:44:42 +0600

    Add root URL handler

--- storage_project/urls.py
+++ storage_project/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        "message": "Storage Management System API is running",
+        "endpoints": {
+            "users": "/api/users/",
+            "inventory": "/api/inventory/",
+            "admin": "/admin/"
+        }
+    })
 
 urlpatterns = [
+    path('', api_root),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/inventory/', include('inventory.urls')),
